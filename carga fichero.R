bdd0cf3d7a3b066f607c496d131ba2f77ea2d34f
leire <leire.legarreta@gmail.com>
2020-05-21 16:37:30 +0200

    Moficamos los nombres de las variables

--- carga fichero.R
+++ carga fichero.R
@@ -1,3 +1,8 @@
 load("C:/Users/llegarreta/Desktop/git/final/examen/muestra03.RData")
 dim(muestra03)
 "La tabla tiene 306085 registros y 5 variables"
+
+library(dplyr)
+muestra03<-select(muestra03,-c("tipo_nif","nif_original"))
+
+colnames(muestra03)<-c("nif","nombre","genero")
