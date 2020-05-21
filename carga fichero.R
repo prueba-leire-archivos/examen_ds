e86b5f4552d0f9b49cc9aa76a8312f011e897d97
leire <leire.legarreta@gmail.com>
2020-05-21 17:09:03 +0200

    eliminamos registros sin genero

--- carga fichero.R
+++ carga fichero.R
@@ -6,3 +6,12 @@ library(dplyr)
 muestra03<-select(muestra03,-c("tipo_nif","nif_original"))
 
 colnames(muestra03)<-c("nif","nombre","genero")
+
+table(muestra03$genero)
+con_genero<-muestra03[muestra03$genero %in% c("M","V"),]
+freqs<-as.data.frame(table(muestra03$nombre,muestra03$genero))
+muestra03<-muestra03[muestra03$genero %in% c("M","V"),]
+
+dim(muestra03)
+"La tabla tiene 137503 registros y 5 variables"
+
