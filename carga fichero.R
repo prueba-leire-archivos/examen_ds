6314fc6d27eb7aa1b9e5d6cd9522a6bdde9b0030
leire <leire.legarreta@gmail.com>
2020-05-21 16:59:34 +0200

    Filtramos nifs erroneos

--- carga fichero.R
+++ carga fichero.R
@@ -6,3 +6,10 @@ library(dplyr)
 muestra03<-select(muestra03,-c("tipo_nif","nif_original"))
 
 colnames(muestra03)<-c("nif","nombre","genero")
+
+muestra03<-muestra03[str_length(muestra03$nif)==9,]
+dim(muestra03)
+"La tabla tiene 299417 registros y 3 variables"
+
+save(muestra03,file="C:/Users/llegarreta/Desktop/git/final/examen/muestra03.RData")
+                
